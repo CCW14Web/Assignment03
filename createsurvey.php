d48caafcb9377714bd98a933a1d5994fc19fcd0e
JohnnyHu9 <r17d09@gmail.com>
2014-04-18 19:43:24 -0400

    folders files created - Johnny
    php files for each function are created

---
+++ createsurvey.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <meta name="robots" content="noindex">
+    <meta name="googlebot" content="noindex">
+    <!--
+        File name : createsurvey.php
+        Author's name : Zhixiang Hu
+        Web site name : Project Survey
+        This is create and save survey to database
+    -->
+
+
+    <title>Project Survey - Create Survey</title>
+    <!--User javascript file-->
+    <link rel="stylesheet" type="text/css" href="css/createsurvey.css">
+
+    <!--jQuery on CND-->
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+    <!--User javascript file-->
+    <script type="text/javascript" src="js/createsurvey.js"></script>
+</head>    
+
+<body>
+    
+    
+    
+    
+</body>
+
+
+</html>
---
+++ js/createsurvey.js
@@ -0,0 +1,6 @@
+/*
+File name : createsurvey.js
+Author's name : Zhixiang Hu
+Web site name : Zhixiang Hu Tech Space
+This is the external javascript file of createsurvey.php
+*/
